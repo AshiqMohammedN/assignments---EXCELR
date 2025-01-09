9c421a67f608977a6876cdec787b6fc575015141
Ashiq MohammedN <149301744+AshiqMohammedN@users.noreply.github.com>
2025-01-09 13:18:46 +0530

    Add files via upload

---
+++ My_module.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+greet = lambda name : print(f"Good Morning {name}")
+greet ("Reventh")
+
+
+# In[2]:
+
+
+product = lambda x,y,z : x*y*z
+product(10,20,30)
+
+
+# In[3]:
+
+
+even = lambda L :[x for x in L if x%2 ==0]
+my_list = [100,9,38,43,56,20]
+even(my_list)
+
+
+# In[ ]:
+
+
+
+
